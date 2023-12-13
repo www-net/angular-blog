c96efffe1ab666b0d041133afa8659d02693aadd
www-net <78052177+www-net@users.noreply.github.com>
2023-12-13 18:22:59 +0300

    feat: post list output

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/shared/interfaces';
+import { PostsService } from 'src/app/shared/posts.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
-  styleUrls: ['./dashboard-page.component.scss']
+  styleUrls: ['./dashboard-page.component.scss'],
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  pSub: Subscription;
 
-  constructor(){}
+  constructor(private postsService: PostsService) {}
 
+  ngOnInit() {
+    this.postsService.getAll().subscribe((posts) => {
+      this.posts = posts;
+    });
+  }
+
+  remove(id: string) {
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
+  }
 }
