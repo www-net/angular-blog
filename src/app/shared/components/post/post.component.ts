693c0eab027b396e071feb367856ff4fab23ebc6
www-net <78052177+www-net@users.noreply.github.com>
2023-12-24 17:48:44 +0300

    feat: post output

--- src/app/shared/components/post/post.component.ts
+++ src/app/shared/components/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from '../../interfaces';
 
 @Component({
   selector: 'app-post',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent {
+
+@Input() post: Post
 
 }
