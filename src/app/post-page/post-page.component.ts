d4632b3675c3d984c07a4b264fea140962d907fa
www-net <78052177+www-net@users.noreply.github.com>
2023-12-24 18:12:05 +0300

    feat: post display

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { PostsService } from '../shared/posts.service';
+import { Observable, switchMap } from 'rxjs';
+import { Post } from '../shared/interfaces';
 
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
   styleUrls: ['./post-page.component.scss']
 })
-export class PostPageComponent {
+export class PostPageComponent implements OnInit{
+
+post$: Observable<Post>
+
+constructor(
+  private route: ActivatedRoute,
+  private postsService: PostsService
+) {}
+
+ngOnInit(): void {
+  this.post$ = this.route.params
+    .pipe(switchMap((params: Params) => {
+      console.log(params)
+      return this.postsService.getById(params['id'])
+    }))
+}
 
 }
